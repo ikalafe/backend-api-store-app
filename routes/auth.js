52a168e6efd8a6773e21b25e3ca2cada016a4c15
Daniyal <daniyaldehghan2021@gmail.com>
2024-08-25 19:46:03 +0330

    Implement user sign-in with JWT authentication
    - Added sign-in endpoint for user authentication
    - Verified user credentials with bcrypt password comparison
    - Generated JWT token for authenticated users
    - Removed password from the response for security purposes
    - Included error handling for invalid credentials

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,9 @@ const express = require("express");
 const User = require("../models/user");
 const authRouter = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+//sign up api endpoint
 authRouter.post("/api/signup", async (req, res) => {
   try {
     const { fullName, email, password } = req.body;
@@ -20,6 +22,30 @@ authRouter.post("/api/signup", async (req, res) => {
       let user = new User({ fullName, email, password: hashedPassword });
       user = await user.save();
       res.json({ user });
+    }
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+//sign in api endpoint
+authRouter.post("/api/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const findUser = await User.findOne({ email });
+    if (!findUser) {
+      return res.status(400).json({ msg: "User Not Found with this email" });
+    } else {
+      const isMatch = await bcrypt.compare(password, findUser.password);
+      if (!isMatch) {
+        res.status(400).json({ msg: "Incorrect password" });
+      } else {
+        const token = jwt.sign({ id: findUser._id }, "passwordKey");
+        //remove sensitive information
+        const { password, ...userWithoutPassword } = findUser._doc;
+        //send the response
+        res.json({ token, ...userWithoutPassword });
+      }
     }
   } catch (e) {
     res.status(500).json({ error: e.message });
