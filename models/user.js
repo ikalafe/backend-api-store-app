168754df53ff1b5554229d2002748ec925bb8963
Daniyal <daniyaldehghan2021@gmail.com>
2024-08-25 05:42:00 +0330

    Add validation for email and password in User model
    - Added custom validation to ensure password is at least 8 characters long
    - Added regex-based validation to ensure email format is valid

--- models/user.js
+++ models/user.js
@@ -11,6 +11,14 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
     trim: true,
+    validate: {
+      validator: (value) => {
+        const result =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return result.test(value);
+      },
+      message: "Please Enter a Valid Email Address",
+    },
   },
 
   state: {
@@ -32,6 +40,13 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
     trim: true,
+    validate: {
+      validator: (value) => {
+        //check if password is at least 8 characters long
+        return value.length >= 8;
+      },
+      message: "Password must be at least 8 characters long",
+    },
   },
 });
 
