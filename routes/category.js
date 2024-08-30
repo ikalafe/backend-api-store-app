c7b50b5d3a77482b7810e02b7ae54540f13cb0ba
Daniyal <daniyaldehghan2021@gmail.com>
2024-08-30 16:34:21 +0330

    Add GET endpoint to retrieve all categories from the database

--- routes/category.js
+++ routes/category.js
@@ -13,4 +13,13 @@ categoryRouter.post("/api/categories", async (req, res) => {
   }
 });
 
+categoryRouter.get("/api/categories", async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json({ categories });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
 module.exports = categoryRouter;
