9f90d1c517fc69e1f6934cbb6dfcbdabe486c4f9
Daniyal <daniyaldehghan2021@gmail.com>
2024-08-30 15:56:29 +0330

    Add a POST endpoint to create a category

---
+++ routes/category.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const Category = require("../models/category");
+const categoryRouter = express.Router();
+
+categoryRouter.post("/api/categories", async (req, res) => {
+  try {
+    const { name, image, banner } = req.body;
+    const category = new Category({ name, image, banner });
+    await category.save();
+    res.status(201).send(category);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+module.exports = categoryRouter;
