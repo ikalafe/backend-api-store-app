71afe28ec26fe96ef2becc94e5545b4e1436aee9
Daniyal <daniyaldehghan2021@gmail.com>
2024-08-30 15:06:13 +0330

    Create schema for product categories

---
+++ models/category.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  banner: {
+    type: String,
+    required: true
+  }
+});
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
