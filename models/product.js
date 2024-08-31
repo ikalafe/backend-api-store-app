f28e71985b28e472f0cac5f5605877900dc4061c
Daniyal <daniyaldehghan2021@gmail.com>
2024-08-31 04:02:17 +0330

    Create Product schema and model
    - Added Product schema with fields for product name, price, quantity, description, category, subcategory, images, and popularity/recommendation flags.
    - Defined a Mongoose model for products to handle database operations.
    - Included default values for popular and recommend fields.

---
+++ models/product.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const productShema = mongoose.Schema({
+  productName: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  productPrice: {
+    type: Number,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  supCategory: {
+    type: String,
+    required: true
+  },
+  images: [
+    {
+      type: String,
+      required: true
+    }
+  ],
+  popular: {
+    type: Boolean,
+    default: true
+  },
+  recommend: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Product = mongoose.model("Product", productShema);
+
+module.exports = Product;
