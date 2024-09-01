3636af43823693d9fd67ef4bd1dcb5fdb7898a58
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-01 14:23:27 +0330

    Add GET endpoint to fetch recommended products
    - Implemented  endpoint to retrieve products marked as recommended (where recommend is true).
    - Returns a 404 error if no recommended products are found, otherwise returns the list of recommend products.

--- routes/product.js
+++ routes/product.js
@@ -29,16 +29,29 @@ productRouter.post("/api/add-product", async (req, res) => {
   }
 });
 
-productRouter.get("/api/popular-product", async (req, res) => {
+productRouter.get("/api/popular-products", async (req, res) => {
   try {
     const product = await Product.find({ popular: true });
     if (!product || product.length == 0) {
-      return res.status(400).json({ msg: "Products Not Found" });
+      return res.status(404).json({ msg: "Products Not Found" });
     } else {
       return res.status(200).json({ product });
     }
   } catch (e) {
     res.status(500).json({ error: e.message });
+  }
+});
+
+productRouter.get("/api/recommended-products", async (req, res) => {
+  try {
+    const product = await Product.find({ recommend: true });
+    if (!product || product.length == 0) {
+      return res.status(404).json({ msg: "Product Not Found" });
+    } else {
+      res.status(200).json({ product });
+    }
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
   }
 });
 
